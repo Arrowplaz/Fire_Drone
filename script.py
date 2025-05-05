879a83ada925151c46d5004ca524847199d930ab
Abhi Reddy <anagireddygari@bowdoin.edu>
2025-05-05 11:21:14 -0400

    t

--- script.py
+++ script.py
@@ -69,7 +69,8 @@ def analyze_roi_with_mask(frame, mask, annotation):
 def preprocess(video_path, output_path='output.mp4', show=False):
     # Open the video file
 
-    output_path = f"./output_videos/output.mp4"
+    output_path = f"./output_videos/"
+    os.makedirs(output_path)
     cap = cv2.VideoCapture(video_path)
     fgbg = cv2.createBackgroundSubtractorKNN()
 
